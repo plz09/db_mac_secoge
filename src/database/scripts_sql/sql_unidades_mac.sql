b57fdc4ec89ca90fe1ae56efced69847464e6048
Ravi Pellizzi <r.pellizzi09@gmail.com>
2024-06-23 10:34:50 -0300

    adicionando script de criacao de unidades_mac e definindo relacionando com distritos

--- src/database/scripts_sql/sql_unidades_mac.sql
+++ src/database/scripts_sql/sql_unidades_mac.sql
@@ -1,7 +1,7 @@
 -- codigo para criar e popular tabela unidades_mac
 
-CREATE TABLE IF NOT EXISTS unidades.unidades_mac (
-		id_unidades_mac Integer,
+CREATE TABLE IF NOT EXISTS ds_unidades.unidades_mac (
+		id_unidades_mac SERIAL PRIMARY KEY,
         cnes_padrao INTEGER,
         codigo_unidade INTEGER,
         distrito INTEGER,
@@ -9,13 +9,26 @@ CREATE TABLE IF NOT EXISTS unidades.unidades_mac (
         tipo_servi CHARACTER VARYING
     );
 
-INSERT INTO unidades.unidades_mac (id_unidades_mac, cnes_padrao, codigo_unidade, distrito, nome, tipo_servi)
+INSERT INTO ds_unidades.unidades_mac (id_unidades_mac, cnes_padrao, codigo_unidade, distrito, nome, tipo_servi)
 SELECT id_unidades, cnes_padrao, no_da_us, ds, nome_fantasia, tipo_servi
-FROM unidades.unidades;
+FROM ds_unidades.unidades;
 
 
-select * from unidades.unidades_mac;
+-- Difinindo FK de Distritos
 
+ALTER TABLE ds_unidades.unidades_mac
+ADD COLUMN fk_distritos INTEGER
+;
+
+UPDATE ds_unidades.unidades_mac tab_mac
+SET fk_distritos = id_distritos
+FROM ds_unidades.distritos tab_ds
+WHERE tab_mac.distrito = tab_ds.ds
+;
+
+ALTER TABLE ds_unidades.unidades_mac
+ADD CONSTRAINT fk_distritos FOREIGN KEY (fk_distritos) REFERENCES ds_unidades.distritos(id_distritos)
+;
   
 
  
