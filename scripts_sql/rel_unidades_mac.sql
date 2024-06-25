9efb64c90fd7a9f97a8c547c110120f6568ef82c
plz09 <r.pellizzi09@gmail.com>
2024-06-25 11:27:00 -0300

    adicionando relacionamento de unidades_mac com spacirurgia

--- scripts_sql/rel_unidades_mac.sql
+++ scripts_sql/rel_unidades_mac.sql
@@ -1,1 +1,22 @@
-ALTER TABLE 
+
+-- Relacionamentos de unidades_mac com SPA
+
+ALTER TABLE spa.spacirurgia
+ADD COLUMN fk_unidades_mac INTEGER
+;
+
+UPDATE spa.spacirurgia spacir 
+SET unidade = nome
+FROM ds_unidades.unidades_mac tab_mac 
+WHERE tab_mac.nome LIKE '%Arnaldo Marques%'
+;
+
+UPDATE spa.spacirurgia spacir 
+SET fk_unidades_mac = id_unidades_mac
+FROM ds_unidades.unidades_mac tab_mac 
+WHERE tab_mac.nome LIKE '%Arnaldo Marques%'
+;
+
+ALTER TABLE spa.spacirurgia
+ADD CONSTRAINT fk_unidades_mac FOREIGN KEY (fk_unidades_mac) REFERENCES ds_unidades.unidades_mac(id_unidades_mac)
+;
