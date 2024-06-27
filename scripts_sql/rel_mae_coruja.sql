4f5a9bb08d672efedb5de89956aa974ce9212c5f
plz09 <r.pellizzi09@gmail.com>
2024-06-27 14:34:17 -0300

    relacionamento de mae_coruja_atividades com mae_coruja.espacos

--- scripts_sql/rel_mae_coruja.sql
+++ scripts_sql/rel_mae_coruja.sql
@@ -33,5 +33,66 @@ ALTER TABLE mae_coruja.espacos_bairros_cobertos
 ADD CONSTRAINT fk_id_distritos FOREIGN KEY (fk_id_distritos) REFERENCES ds_unidades.distritos(id_distritos)
 ;
 
--- Relacionamento espacos_unidades com distritos
+-- Relacionamento espacos_unidades com unidades_mac
+
+
+ALTER TABLE mae_coruja.espacos_unidades
+ADD COLUMN fk_id_unidades_mac INTEGER
+;
+
+UPDATE mae_coruja.espacos_unidades espac_uni 
+SET fk_id_unidades_mac = tab_mac.id_unidades_mac
+FROM ds_unidades.unidades_mac tab_mac 
+WHERE espac_uni.usf_cnes = tab_mac.cnes_padrao 
+;
+
+ALTER TABLE mae_coruja.espacos_unidades
+ADD CONSTRAINT fk_id_unidades_mac FOREIGN KEY (fk_id_unidades_mac) REFERENCES ds_unidades.unidades_mac(id_unidades_mac)
+;
+
+-- Dropando colunas da tabela mae_coruja_atividades
+
+DO $$
+DECLARE
+    column_name text;
+    columns_to_keep text[] := array[
+        'id_mae_coruja_atividades', 'no', 'secretaria', 'espaco', 'acao', 'data_inicio', 
+        'data_termino', 'presentes', 'convidados', 'observacao', 
+        'sintese_da_acao', 'equipe_tecnica'
+    ];
+    drop_columns_query text := 'ALTER TABLE mae_coruja.mae_coruja_atividades ';
+BEGIN
+    FOR column_name IN 
+        SELECT col.column_name
+        FROM information_schema.columns col
+        WHERE col.table_name = 'mae_coruja_atividades' 
+        AND col.table_schema = 'mae_coruja'
+        AND col.column_name <> ALL(columns_to_keep)
+    LOOP
+        drop_columns_query := drop_columns_query || 'DROP COLUMN "' || column_name || '", ';
+    END LOOP;
+
+    -- Remove the última vírgula e espaço
+    drop_columns_query := left(drop_columns_query, length(drop_columns_query) - 2) || ';';
+
+    -- Execute a query dinâmica
+    EXECUTE drop_columns_query;
+END $$;
+
+
+-- Relacionamento de mae_coruja_atividades com mae_coruja.espacos
+
+ALTER TABLE mae_coruja.mae_coruja_atividades
+ADD COLUMN fk_id_espacos INTEGER
+;
+
+UPDATE mae_coruja.mae_coruja_atividades mae_co_ativd 
+SET fk_id_espacos = mae_co_espacos.id_espacos
+FROM mae_coruja.espacos mae_co_espacos  
+WHERE mae_co_ativd.espaco LIKE CONCAT('%', mae_co_espacos.espaco, '%')
+;
+
+ALTER TABLE mae_coruja.mae_coruja_atividades
+ADD CONSTRAINT fk_id_espacos FOREIGN KEY (fk_id_espacos) REFERENCES mae_coruja.espacos(id_espacos)
+;
 
--- src/data_processing/mae_coruja.py
+++ src/data_processing/mae_coruja.py
@@ -6,16 +6,21 @@ def process_mae_coruja_data(engine):
     path_mae_coruja_mulher = 'data_bruto/Mae_Coruja/DADOS MULHER.xlsx'
     path_mae_coruja_crianca = 'data_bruto/Mae_Coruja/DADOS CRIANÇA.xlsx'
     path_mae_coruja_kits = 'data_bruto/Mae_Coruja/consolidado_kits_pmcr.xlsx'
+    path_mae_coruja_atividades = 'data_bruto/Mae_Coruja/consolidado_atividades_coletivas_pmcr.xlsx'
+    aba_atividades = 'atv_cltv_2018_2023'
     aba_2024_mae_coruja_kits = '2024'
 
     df_mae_coruja_mulher = remove_espacos_e_acentos(path_mae_coruja_mulher)
     df_mae_coruja_crianca = remove_espacos_e_acentos(path_mae_coruja_crianca)
     df_mae_coruja_kits = remove_espacos_e_acentos(path_mae_coruja_kits, aba_selecionada=aba_2024_mae_coruja_kits)
 
+    df_mae_coruja_atividades = remove_espacos_e_acentos(path_mae_coruja_atividades, aba_selecionada=aba_atividades, skip_rows=11)
+
     data = {
         'mae_coruja_mulher': df_mae_coruja_mulher,
         'mae_coruja_crianca': df_mae_coruja_crianca,
-        'mae_coruja_kits_aba_2024': df_mae_coruja_kits
+        'mae_coruja_kits_aba_2024': df_mae_coruja_kits,
+        'mae_coruja_atividades': df_mae_coruja_atividades
     }
 
     schema = 'mae_coruja'
@@ -33,11 +38,11 @@ def process_mae_coruja_data(engine):
 
     for aba in abas_mae_coruja_espacos:
         if aba == 'ESPACOS':
-            colunas_definidas = ['ESPAÇO', 'DS', 'SIGLA', 'LOCAL', 'ENDEREÇO', 'BAIRROS COBERTOS',
+            colunas_mae_co_espacos = ['ESPAÇO', 'DS', 'SIGLA', 'LOCAL', 'ENDEREÇO', 'BAIRROS COBERTOS',
                                  'UNIDADES COBERTAS', 'DATA INAUGURAÇÃO ESPAÇO PMCR']
             try:
                 df_mae_coruja_espacos = remove_espacos_e_acentos(
-                    path_mae_coruja_espacos, aba_selecionada=aba, skip_rows=5, colunas=colunas_definidas)
+                    path_mae_coruja_espacos, aba_selecionada=aba, skip_rows=5, colunas=colunas_mae_co_espacos)
             except Exception as e:
                 print(f"Erro ao processar a aba {aba}: {e}")
                 continue  # pula para a próxima iteração se ocorrer um erro
