c0f03e955599398e0b136dfb166451eec9d9fe05
plz09 <r.pellizzi09@gmail.com>
2024-06-27 13:26:23 -0300

    relacionamento de espacos_cobertos com distritos

--- scripts_sql/rel_mae_coruja.sql
+++ scripts_sql/rel_mae_coruja.sql
@@ -16,3 +16,22 @@ WHERE REPLACE(mae_co_espacos.ds, ' ', '') = ds.ds_romano
 ALTER TABLE mae_coruja.espacos
 ADD CONSTRAINT fk_id_distritos FOREIGN KEY (fk_id_distritos) REFERENCES ds_unidades.distritos(id_distritos)
 ;
+
+-- Relacionamento de mae_coruja.espacos_bairros_cobertos com ds_unidades.distritos
+
+ALTER TABLE mae_coruja.espacos_bairros_cobertos
+ADD COLUMN fk_id_distritos INTEGER
+;
+
+UPDATE mae_coruja.espacos_bairros_cobertos mae_co_espac_cobertos 
+SET fk_id_distritos = ds.id_distritos 
+FROM ds_unidades.distritos ds 
+WHERE REPLACE(mae_co_espac_cobertos.ds, ' ', '') = ds.ds_romano 
+;
+
+ALTER TABLE mae_coruja.espacos_bairros_cobertos
+ADD CONSTRAINT fk_id_distritos FOREIGN KEY (fk_id_distritos) REFERENCES ds_unidades.distritos(id_distritos)
+;
+
+-- Relacionamento espacos_unidades com distritos
+
