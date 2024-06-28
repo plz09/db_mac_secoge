9f9a16963e947fc59621c26ad581ad9b8af3dedd
plz09 <r.pellizzi09@gmail.com>
2024-06-28 12:28:20 -0300

    finalizando relacionamentos producao

--- scripts_sql/rel_producao.sql
+++ scripts_sql/rel_producao.sql
@@ -1,4 +1,41 @@
--- Relacionamento do schema -producao entre  si entre e com unidades_mac
+-- Relacionamento do schema producao entre  si entre e com unidades_mac
+
+-- Unificando dcbo com dport157
+
+-- Verificando valores faltantes em dcbo que existem em dport157
+
+SELECT 
+    dcbo.cbo AS dcbo_cbo,
+    CAST(dport.cbo AS TEXT) AS dport_cbo,
+	dport.especialidade
+FROM 
+    producao.dcbo dcbo
+RIGHT JOIN
+    producao.dport157 dport
+ON 
+    dcbo.cbo = CAST(dport.cbo AS TEXT)
+;
+
+-- Inserindo valores faltantes
+
+INSERT INTO producao.dcbo (cbo, ds_cbo)
+VALUES 
+('223117', 'MEDICO DERMATOLOGISTA HANSENOLOGO'),
+('223142', 'MEDICO NEUROPEDIATRA')
+;
+
+ALTER TABLE producao.dcbo
+ADD COLUMN procedimentos INTEGER
+;
+
+UPDATE producao.dcbo dcbo
+SET procedimentos = dport.procedimentos
+FROM producao.dport157 dport
+WHERE dcbo.cbo = CAST(dport.cbo AS TEXT)
+;
+
+DROP TABLE producao.dport157
+;
 
 -- Rel fproducao2024 com dcbo
 
@@ -32,7 +69,7 @@ ALTER TABLE producao.fproducao2024
 ADD CONSTRAINT fk_id_unidades_mac FOREIGN KEY (fk_id_unidades_mac) REFERENCES ds_unidades.unidades_mac(id_unidades_mac)
 ;
 
--- Rel fproducao2024 com formaorganiz
+-- Rel fproducao2024 com dformaorganiz
 
 ALTER TABLE producao.fproducao2024
 ADD COLUMN fk_id_dformaorganiz INTEGER
@@ -82,40 +119,34 @@ ALTER TABLE producao.fprofissionais
 ADD CONSTRAINT fk_id_dcbo FOREIGN KEY (fk_id_dcbo) REFERENCES producao.dcbo(id_dcbo)
 ;
 
+-- Rel dextrato_profissionais_sus com unidades_mac
 
--- Unificando dcbo com dport157
-
--- Verificando valores faltantes em dcbo que existem em dport157
-
-SELECT 
-    dcbo.cbo AS dcbo_cbo,
-    CAST(dport.cbo AS TEXT) AS dport_cbo,
-	dport.especialidade
-FROM 
-    producao.dcbo dcbo
-RIGHT JOIN
-    producao.dport157 dport
-ON 
-    dcbo.cbo = CAST(dport.cbo AS TEXT)
+ALTER TABLE producao.dextrato_profissionais_sus
+ADD COLUMN fk_id_unidades_mac INTEGER
 ;
 
--- Inserindo valores faltantes
-
-INSERT INTO producao.dcbo (cbo, ds_cbo)
-VALUES 
-('223117', 'MEDICO DERMATOLOGISTA HANSENOLOGO'),
-('223142', 'MEDICO NEUROPEDIATRA')
+UPDATE producao.dextrato_profissionais_sus dextrat
+SET fk_id_unidades_mac = id_unidades_mac
+FROM ds_unidades.unidades_mac tab_mac
+WHERE tab_mac.cnes_padrao = dextrat.cnes
 ;
 
--- Insrindo coluna procedimentos
-
-ALTER TABLE producao.dcbo
-ADD COLUMN procedimentos INTEGER
+ALTER TABLE producao.dextrato_profissionais_sus
+ADD CONSTRAINT fk_id_unidades_mac FOREIGN KEY (fk_id_unidades_mac) REFERENCES ds_unidades.unidades_mac(id_unidades_mac)
 ;
 
-UPDATE producao.dcbo dcbo
-SET procedimentos = dport.procedimentos
-FROM producao.dport157 dport
-WHERE dcbo.cbo = CAST(dport.cbo AS TEXT)
+-- Rel dextrato_profissionais_sus com unidades_mac com dcbo
+
+ALTER TABLE producao.dextrato_profissionais_sus
+ADD COLUMN fk_id_dcbo INTEGER
 ;
 
+UPDATE producao.dextrato_profissionais_sus dextrat
+SET fk_id_dcbo = id_dcbo 
+FROM producao.dcbo dcbo
+WHERE dcbo.cbo = dextrat.profissional_cbo
+;
+
+ALTER TABLE producao.dextrato_profissionais_sus
+ADD CONSTRAINT fk_id_dcbo FOREIGN KEY (fk_id_dcbo) REFERENCES producao.dcbo(id_dcbo)
+;
--- src/data_processing/producao.py
+++ src/data_processing/producao.py
@@ -6,17 +6,20 @@ def read_producao_data():
     path_dCBO = 'data_bruto/producao/dCBO.xlsx'
     path_dPort157 = 'data_bruto/producao/dPort157.xlsx'
     path_fproducao2024 = 'data_bruto/producao/fProducao2024.csv'
+    path_dextrato_profissionais_sus = 'data_bruto/producao/dExtrato_Profissioais_SUS.xlsx'
 
     df_dFormaOrganiz = remove_espacos_e_acentos(path_dFormaOrganiz)
     df_fProfissionais = remove_espacos_e_acentos(path_fProfissionais)
     df_dCBO = remove_espacos_e_acentos(path_dCBO)
     df_dPort157 = remove_espacos_e_acentos(path_dPort157)
     df_fproducao2024 = remove_espacos_e_acentos(path_fproducao2024, dtype={3: str})
+    df_dextrato_profissionais_sus = remove_espacos_e_acentos(path_dextrato_profissionais_sus)
 
     return {
         'dformaorganiz': df_dFormaOrganiz,
         'fprofissionais': df_fProfissionais,
         'dcbo': df_dCBO,
         'dport157': df_dPort157,
-        'fproducao2024': df_fproducao2024
+        'fproducao2024': df_fproducao2024,
+        'dextrato_profissionais_sus': df_dextrato_profissionais_sus
     }
